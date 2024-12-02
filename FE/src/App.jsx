378488d70117ab860bf007e5330e81d79a1907d8
Phong908 <122242560+khanhvo908@users.noreply.github.com>
2024-12-02 13:36:53 +0700

    chinh HOME

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './assets/JSX/Home';
 
 function App() {
   return (
     <Router>
       <nav>
-      
+      <Home/>
       </nav>
       <Routes>
-     
+    
       </Routes>
     </Router>
   );
